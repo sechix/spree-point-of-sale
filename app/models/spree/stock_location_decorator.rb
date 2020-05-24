0e885b6b2549b88c684e31a87b7b1dc0538fb943
sechix <sechix@gmail.com>
2020-05-24 19:50:37 +0200

    Update stock_location_decorator.rb

--- app/models/spree/stock_location_decorator.rb
+++ app/models/spree/stock_location_decorator.rb
@@ -1,8 +1,5 @@
 module Spree
   StockLocation.class_eval do
-    belongs_to :address
-
-    before_validation :associate_address
 
     scope :stores, -> { where(store: true) }
     scope :not_store, -> { where.not(store: true) }
@@ -16,15 +13,5 @@ module Spree
     def active_store?
       active? && store?
     end
-
-    private
-      def associate_address
-        if respond_to?(:state_id) && store?
-          self.address = Spree::Address.find_or_initialize_by(firstname: name,
-            lastname: '(Store)', state_id: state_id, country_id: country_id,
-            address1: address1, address2: address2, phone: phone, zipcode: zipcode,
-            city: city)
-        end
-      end
   end
 end
